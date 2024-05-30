541edd1a7cf1bbdeb10d10e9475e93696bf5a2c1
Claudiu Jipa <89317993+jipelski@users.noreply.github.com>
2024-05-30 20:44:04 +0100

    Added leetcode counter and respective fetcher using js

--- js/main.js
+++ js/main.js
@@ -17,13 +17,32 @@ function loadContent(page) {
 }
 
 function updateCounters() {
+    fetchLeetCode("jipelski")
+        .then(r => {
+            document.getElementById('leetcode-total-counter').innerHTML = r;
+        })
+        .catch(e => {
+            console.error("Error fetching leetcode data",e);
+            document.getElementById('leetcode-total-counter').innerHTML = 'Error';
+        })
+}
 
+async function fetchLeetCode(username){
+    const response = await fetch(`https://leetcode-stats-api.herokuapp.com/${username}`);
+
+    if (!response.ok) {
+        throw new Error(response.statusText);
+    }
+
+    const data = await response.json();
+    console.log(data.totalSolved);
+    return data.totalSolved;
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     loadHeader();
     loadContent('/src/home.html');
-    updateCounters();
 
     window.loadContent = loadContent;
+    window.updateCounters = updateCounters;
 })

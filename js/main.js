df8e61f936076165edffe46a95cb120eb7af7bd7
Claudiu Jipa <89317993+jipelski@users.noreply.github.com>
2024-05-30 21:04:11 +0100

    removed leetcode fetcher to adhere to terms of service

--- js/main.js
+++ js/main.js
@@ -16,33 +16,9 @@ function loadContent(page) {
         .catch(error => console.error('Error loading the content of ' + page, error));
 }
 
-function updateCounters() {
-    fetchLeetCode("jipelski")
-        .then(r => {
-            document.getElementById('leetcode-total-counter').innerHTML = r;
-        })
-        .catch(e => {
-            console.error("Error fetching leetcode data",e);
-            document.getElementById('leetcode-total-counter').innerHTML = 'Error';
-        })
-}
-
-async function fetchLeetCode(username){
-    const response = await fetch(`https://leetcode-stats-api.herokuapp.com/${username}`);
-
-    if (!response.ok) {
-        throw new Error(response.statusText);
-    }
-
-    const data = await response.json();
-    console.log(data.totalSolved);
-    return data.totalSolved;
-}
-
 document.addEventListener('DOMContentLoaded', () => {
     loadHeader();
     loadContent('/src/home.html');
 
     window.loadContent = loadContent;
-    window.updateCounters = updateCounters;
 })

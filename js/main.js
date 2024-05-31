a77ec820f674a54509d08354990f2bfab1dd71ff
Claudiu Jipa <89317993+jipelski@users.noreply.github.com>
2024-05-31 09:54:18 +0100

    improved behaviour of contact form by integrating input validation

--- js/main.js
+++ js/main.js
@@ -26,16 +26,31 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 function sendForm() {
-    event.preventDefault();
-    emailjs.init('I6XAeZBxH6tn7Dqg3')
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
 
-    const serviceID = 'service_dvofvzg';
-    const templateID = 'template_1qbyhbf';
+    if (name === '') {
+        alert("Please enter your name");
+    }
+    else if(email === '' || !email.includes("@") || !email.includes(".")) {
+        alert("Please enter a valid email");
+    }
+    else if(message === '') {
+        alert("Please enter your message");
+    }
+    else{
+        event.preventDefault()
+        emailjs.init('I6XAeZBxH6tn7Dqg3')
 
-    emailjs.sendForm(serviceID, templateID, '#contact-form')
-        .then(() => {
-            alert('Sent!');
-        }, (err) => {
-            alert(JSON.stringify(err));
-        });
+        const serviceID = 'service_dvofvzg';
+        const templateID = 'template_1qbyhbf';
+
+        emailjs.sendForm(serviceID, templateID, "#contact_form")
+            .then(() => {
+                alert('Sent!');
+            }, (err) => {
+                alert(JSON.stringify(err));
+            });
+    }
 }
